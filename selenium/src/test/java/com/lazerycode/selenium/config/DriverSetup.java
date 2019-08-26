b89638e27b07b9e5993029b43e3d17a79d12ea48
root <root@master.us-east4-c.c.hub-kubernetes.internal>
2019-08-26 14:31:46 +0000

    adding selenium tests for angular project

---
+++ selenium/src/test/java/com/lazerycode/selenium/config/DriverSetup.java
@@ -0,0 +1,8 @@
+package com.lazerycode.selenium.config;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public interface DriverSetup {
+    RemoteWebDriver getWebDriverObject(DesiredCapabilities capabilities);
+}
